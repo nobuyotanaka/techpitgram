c1581b64b222d7e5eaa2397363b0173c69251067
nobuyotanaka <kshiris1028@gmail.com>
2023-11-02 17:43:58 +0900

    モデル同士を関連づける

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,7 @@
 class Like < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+
+  #ユーザーが同じ投稿に対して「いいね」できないようにする
+  validates :user_id, uniqueness: {scope: :post_id}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
 
   belongs_to :user
   has_many :photos, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   accepts_nested_attributes_for :photos
   # 関連付けしたphotoモデルを一緒にデータ保存できる
